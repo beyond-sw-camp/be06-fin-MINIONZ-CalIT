6f5d5ddf614dd7acf719ac73db5357766464c3fe
yunseul <siddl333@naver.com>
2024-09-05 18:16:52 +0900

    [Feat] USER_001 - 시큐리티 컨피그 구현

--- backend/src/main/java/minionz/backend/config/SecurityConfig.java
+++ backend/src/main/java/minionz/backend/config/SecurityConfig.java
@@ -1,4 +1,25 @@
 package minionz.backend.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((auth) -> auth.disable());
+        http.httpBasic((auth) -> auth.disable());
+        http.authorizeHttpRequests((auth) ->
+                auth
+                        .anyRequest().permitAll()
+        );
+
+        return http.build();
+
+    }
 }
