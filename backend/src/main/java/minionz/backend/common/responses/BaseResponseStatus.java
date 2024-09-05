27bd31ef80668bef45d7e8ef0a317f655c8a76da
yunseul <siddl333@naver.com>
2024-09-05 17:56:24 +0900

    [Refactor] 누락된 BaseResponseStatus 추가

---
+++ backend/src/main/java/minionz/backend/common/responses/BaseResponseStatus.java
@@ -0,0 +1,68 @@
+package minionz.backend.common.responses;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+
+    /**
+     * 1000 : 공통 에러
+     */
+
+
+
+    /**
+     * 2000: 유저
+     */
+
+
+
+
+    /**
+     * 3000: 마이 페이지
+     */
+    MY_WORKSPACE_READ_SUCCESS(false, 3001, "내가 참여한 워크스페이스 목록 조회를 성공했습니다."),
+
+
+
+    /**
+     * 4000: 워크스페이스
+     */
+    WORKSPACE_CREATE_SUCCESS(false, 4001, "워크스페이스 생성을 성공했습니다."),
+
+
+    WORKSPACE_ACCESS_DENIED(false, 4101, "워크스페이스에 접근 권한이 없습니다.");
+
+
+    /**
+     * 5000: 게시판
+     */
+
+
+
+    /**
+     * 6000: 채팅
+     */
+
+
+
+
+    /**
+     * 7000: 알림
+     */
+
+
+
+
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
