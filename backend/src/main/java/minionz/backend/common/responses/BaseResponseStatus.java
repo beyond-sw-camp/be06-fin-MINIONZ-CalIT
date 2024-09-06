e7ea1db6c895a12a426a7881a134847b249b5afb
yunseul <siddl333@naver.com>
2024-09-06 12:35:54 +0900

    [Feat] WORKSPACE_009 - 라벨 생성에 필요한 응답 메세지 구현

--- backend/src/main/java/minionz/backend/common/responses/BaseResponseStatus.java
+++ backend/src/main/java/minionz/backend/common/responses/BaseResponseStatus.java
@@ -9,7 +9,7 @@ public enum BaseResponseStatus {
      * 1000 : 공통 에러
      */
     FILE_UPLOAD_FAIL(false, 1001, "파입 업로드에 실패했습니다."),
-
+    INVALID_ACCESS(false, 1002, "비정상적인 접근입니다."),
 
 
     /**
@@ -22,17 +22,21 @@ public enum BaseResponseStatus {
     /**
      * 3000: 마이 페이지
      */
-    MY_WORKSPACE_READ_SUCCESS(false, 3001, "내가 참여한 워크스페이스 목록 조회를 성공했습니다."),
+    MY_WORKSPACE_READ_SUCCESS(true, 3001, "나의 워크스페이스 목록 조회를 성공했습니다."),
 
 
 
     /**
      * 4000: 워크스페이스
      */
-    WORKSPACE_CREATE_SUCCESS(false, 4001, "워크스페이스 생성을 성공했습니다."),
+    WORKSPACE_CREATE_SUCCESS(true, 4001, "워크스페이스 생성을 성공했습니다."),
+    SPRINT_LABEL_CREATE_SUCCESS(true, 4002, "스프린트 라벨 생성을 성공했습니다."),
+    TASK_LABEL_CREATE_SUCCESS(true, 4003, "태스크 라벨 생성을 성공했습니다."),
 
 
     WORKSPACE_ACCESS_DENIED(false, 4101, "워크스페이스에 접근 권한이 없습니다."),
+    WORKSPACE_NOT_EXISTS(false, 4102, "존재하지 않는 워크스페이스입니다."),
+    LABEL_ALREADY_EXISTS(false, 4103, "동일한 라벨 이름이 존재합니다."),
 
 
     /**
