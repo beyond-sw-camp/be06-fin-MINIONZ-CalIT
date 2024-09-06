54e40418c5928ce63f3c339323de3bd46bb5039b
yunseul <siddl333@naver.com>
2024-09-06 19:24:32 +0900

    [Feat] SPRINT_002 - 스프린트 단일 조회 기능 구현

--- backend/src/main/java/minionz/backend/scrum/sprint/SprintController.java
+++ backend/src/main/java/minionz/backend/scrum/sprint/SprintController.java
@@ -1,14 +1,13 @@
 package minionz.backend.scrum.sprint;
 
 import lombok.RequiredArgsConstructor;
+import minionz.backend.common.exception.BaseException;
 import minionz.backend.common.responses.BaseResponse;
 import minionz.backend.common.responses.BaseResponseStatus;
 import minionz.backend.scrum.sprint.model.request.CreateSprintRequest;
+import minionz.backend.scrum.sprint.model.response.ReadSprintResponse;
 import minionz.backend.user.model.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +17,29 @@ public class SprintController {
     private final SprintService sprintService;
 
     @PostMapping("")
-    public BaseResponse<BaseResponseStatus> craeteSprint(@RequestBody CreateSprintRequest request){
+    public BaseResponse<BaseResponseStatus> craeteSprint(@RequestBody CreateSprintRequest request) {
         User user = User.builder().userId(1L).build();
 
-        sprintService.createSprint(user, request);
+        try {
+            sprintService.createSprint(user, request);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
 
         return new BaseResponse<>(BaseResponseStatus.SPRINT_CREATE_SUCCESS);
+    }
+
+    @GetMapping("/{sprintId}")
+    public BaseResponse<ReadSprintResponse> readSprint(@PathVariable Long sprintId) {
+        User user = User.builder().userId(1L).build();
+        ReadSprintResponse response;
+
+        try {
+            response = sprintService.readSprint(user, sprintId);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+
+        return new BaseResponse<>(BaseResponseStatus.SPRINT_READ_SUCCESS, response);
     }
 }
--- backend/src/main/java/minionz/backend/scrum/sprint/SprintService.java
+++ backend/src/main/java/minionz/backend/scrum/sprint/SprintService.java
@@ -9,6 +9,9 @@ import minionz.backend.scrum.label_select.model.SprintLabelSelect;
 import minionz.backend.scrum.sprint.model.Sprint;
 import minionz.backend.scrum.sprint.model.SprintStatus;
 import minionz.backend.scrum.sprint.model.request.CreateSprintRequest;
+import minionz.backend.scrum.sprint.model.response.Label;
+import minionz.backend.scrum.sprint.model.response.Participant;
+import minionz.backend.scrum.sprint.model.response.ReadSprintResponse;
 import minionz.backend.scrum.sprint_participation.SprintParticipationRepository;
 import minionz.backend.scrum.sprint_participation.model.SprintParticipation;
 import minionz.backend.scrum.workspace.WorkspaceRepository;
@@ -16,6 +19,9 @@ import minionz.backend.scrum.workspace.model.Workspace;
 import minionz.backend.scrum.workspace_participation.WorkspaceParticipationRepository;
 import minionz.backend.user.model.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,10 +32,10 @@ public class SprintService {
     private final WorkspaceRepository workspaceRepository;
     private final WorkspaceParticipationRepository workspaceParticipationRepository;  // final이 없으면 초기화 안됨. RequiredArgsConstructor로 의존성 주입 안됨. like const
 
-    public void createSprint(User user, CreateSprintRequest request) {
+    public void createSprint(User user, CreateSprintRequest request) throws BaseException {
 //        user가 워크스페이스 내에 포함됐는지? 확인.
         workspaceParticipationRepository.findByWorkspaceWorkspaceIdAndUserUserId(request.getWorkspaceId(), user.getUserId()).orElseThrow(
-                () ->  new BaseException(BaseResponseStatus.INVALID_ACCESS)
+                () -> new BaseException(BaseResponseStatus.INVALID_ACCESS)
         );
 
         Sprint sprint = sprintRepository.save(Sprint
@@ -61,5 +67,48 @@ public class SprintService {
                         .sprintLabel(SprintLabel.builder().sprintLabelId(labelId).build())
                         .sprint(sprint)
                         .build()));
+    }
+
+    public ReadSprintResponse readSprint(User user, Long id) {
+        Optional<Sprint> result = sprintRepository.findById(id);
+
+        if (result.isEmpty()) {
+            throw new BaseException(BaseResponseStatus.INVALID_ACCESS);
+        }
+
+        Sprint sprint = result.get();
+
+        //        user가 워크스페이스 내에 포함됐는지? 확인.
+        workspaceParticipationRepository.findByWorkspaceWorkspaceIdAndUserUserId(sprint.getWorkspace().getWorkspaceId(), user.getUserId()).orElseThrow(
+                () -> new BaseException(BaseResponseStatus.INVALID_ACCESS)
+        );
+
+        List<Participant> participants = sprint.getSprintParticipations().stream().map(
+                participant -> Participant.builder().id(participant.getUser().getUserId()).userName(participant.getUser().getUserName()).isManager(participant.getIsManager()).build()
+        ).toList();
+
+        List<Label> labels = sprint.getSprintLabelSelects().stream().map(
+                label -> Label
+                        .builder()
+                        .id(label.getSprintLabel().getSprintLabelId())
+                        .labelName(label.getSprintLabel().getLabelName())
+                        .color(label.getSprintLabel().getColor())
+                        .build()
+        ).toList();
+
+//        내가 isManager 이니?
+        Boolean isManager = participants.stream()
+                .anyMatch(participant -> participant.getId().equals(user.getUserId()) && participant.getIsManager());
+
+        return ReadSprintResponse
+                .builder()
+                .sprintId(sprint.getSprintId())
+                .contents(sprint.getSprintContents())
+                .labels(labels)
+                .startDate(sprint.getStartDate())
+                .endDate(sprint.getEndDate())
+                .participants(participants)
+                .isManager(isManager)
+                .build();
     }
 }
