cdb5826284d9f0241d6e56bdcb222f9392855349
yunseul <siddl333@naver.com>
2024-09-06 12:42:58 +0900

    [Fix] 잘못된 문법 수정

--- backend/src/main/java/minionz/backend/error_board/model/ErrorBoard.java
+++ backend/src/main/java/minionz/backend/error_board/model/ErrorBoard.java
@@ -3,7 +3,7 @@ package minionz.backend.error_board.model;
 import jakarta.persistence.*;
 import lombok.*;
 import minionz.backend.error_comment.model.ErrorComment;
-import minionz.backend.scrum.issue.model.Issue;
+import minionz.backend.scrum.task.model.Task;
 import minionz.backend.scrum.workspace.model.Workspace;
 import minionz.backend.user.model.User;
 import org.springframework.data.annotation.CreatedDate;
@@ -43,15 +43,15 @@ public class ErrorBoard {
     private List<ErrorBoardImage> errorBoardImageList= new ArrayList<>();
 
     @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId")
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    @JoinColumn(name = "issueId")
-    private Issue issue;
+    @JoinColumn(name = "task_id")
+    private Task task;
 
     @OneToOne
-    @JoinColumn(name = "workspaceId")
+    @JoinColumn(name = "workspace_id")
     private Workspace workSpace;
 
 
