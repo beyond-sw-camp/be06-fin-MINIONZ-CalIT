f3860939915c6a29b13fe1a3d98d738f6756c591
yunseul <siddl333@naver.com>
2024-09-09 17:11:33 +0900

    [Feat] MEETING_002 - 회의 단일 조회 기능 구현

--- backend/src/main/java/minionz/backend/scrum/meeting/MeetingService.java
+++ backend/src/main/java/minionz/backend/scrum/meeting/MeetingService.java
@@ -1,14 +1,21 @@
 package minionz.backend.scrum.meeting;
 
 import lombok.RequiredArgsConstructor;
+import minionz.backend.common.exception.BaseException;
+import minionz.backend.common.responses.BaseResponseStatus;
 import minionz.backend.scrum.meeting.model.Meeting;
 import minionz.backend.scrum.meeting.model.request.CreateMeetingRequest;
+import minionz.backend.scrum.meeting.model.response.ReadMeetingResponse;
 import minionz.backend.scrum.meeting_participation.MeetingParticipationRepository;
 import minionz.backend.scrum.meeting_participation.model.MeetingParticipation;
 import minionz.backend.scrum.sprint.model.Sprint;
+import minionz.backend.scrum.sprint.model.response.Participant;
 import minionz.backend.user.model.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -41,6 +48,36 @@ public class MeetingService {
 
 //        TODO: 알람 보내야 합니다.
 
+    }
 
+    public ReadMeetingResponse readMeeting(Long meetingId) throws BaseException {
+        Optional<Meeting> result = meetingRepository.findById(meetingId);
+
+        if (result.isEmpty()) {
+            throw new BaseException(BaseResponseStatus.MEETING_NOT_EXISTS);
+        }
+        Meeting meeting = result.get();
+
+        return ReadMeetingResponse
+                .builder()
+                .id(meeting.getMeetingId())
+                .title(meeting.getMeetingTitle())
+                .contents(meeting.getMeetingContents())
+                .startDate(meeting.getStartDate())
+                .endDate(meeting.getEndDate())
+                .createdAt(meeting.getCreatedAt())
+                .participants(findParticipants(meeting))
+                .build();
+    }
+
+
+    public List<Participant> findParticipants(Meeting meeting) {
+        return meeting.getMeetingParticipations().stream().map(
+                participant -> Participant.builder()
+                        .id(participant.getUser().getUserId())
+                        .userName(participant.getUser().getUserName())
+                        .isManager(true)
+                        .build()
+        ).toList();
     }
 }
