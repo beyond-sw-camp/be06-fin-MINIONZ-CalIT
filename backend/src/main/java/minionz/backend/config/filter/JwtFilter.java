137a440ad044d4bcb06546fef641bb6bd80fce4a
seongxun <rjqlrl30@naver.com>
2024-09-09 10:22:00 +0900

    [Feat] USER_003 - 일반 로그인 기능 [#14]

--- backend/src/main/java/minionz/backend/config/filter/JwtFilter.java
+++ backend/src/main/java/minionz/backend/config/filter/JwtFilter.java
@@ -1,4 +1,69 @@
 package minionz.backend.config.filter;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import minionz.backend.Utils.JwtUtil;
+import minionz.backend.user.UserService;
+import minionz.backend.user.model.CustomSecurityUserDetails;
+import minionz.backend.user.model.User;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws  ServletException, IOException, InternalAuthenticationServiceException {
+        try{
+            String authorization = null;
+            System.out.println(request.getCookies());
+            if(request.getCookies() != null) {
+                for (Cookie cookie : request.getCookies()) {
+                    if (cookie.getName().equals("ATOKEN")) {
+                        authorization = cookie.getValue();
+                    }
+                }
+            }
+            if(authorization == null){
+                log.info("인증 쿠키 없음");
+                filterChain.doFilter(request, response);
+                return;
+            }
+            String token = authorization;
+            if(jwtUtil.isExpired(token)){
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            System.out.println(jwtUtil);
+            Long userId = jwtUtil.getUserId(token);
+            String loginId = jwtUtil.getLoginId(token);
+            String email = jwtUtil.getEmail(token);
+            String role = jwtUtil.getRole(token);
+            User user = User.builder()
+                    .userId(userId)
+                    .loginId(loginId)
+                    .email(email)
+                    .role(role)
+                    .build();
+            log.info(loginId + " " + email + " " + role);
+            CustomSecurityUserDetails customUserDetails = new CustomSecurityUserDetails(user);
+            Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+            filterChain.doFilter(request, response);
+        }catch (ServletException e){
+            throw new ServletException(e);
+        }
+    }
 }
