1f70d7c6d3f3dbe6b2c0e1156d5a803bcc50a87b
yunseul <siddl333@naver.com>
2024-09-10 17:23:16 +0900

    [Feat] SCHEDULE_002 - 워크스페이스 주별 캘린더 조회에 필요한 jpql 쿼리 구현

--- backend/src/main/java/minionz/backend/scrum/issue/IssueRepository.java
+++ backend/src/main/java/minionz/backend/scrum/issue/IssueRepository.java
@@ -1,7 +1,16 @@
 package minionz.backend.scrum.issue;
 
 import minionz.backend.scrum.issue.model.Issue;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+    @Query("SELECT i FROM Issue i " +
+            "JOIN i.workspace w " +
+            "WHERE w.workspaceId = :workspaceId " +
+            "AND i.status = true ")
+    List<Issue> findUpcomingIssues(Long workspaceId, Pageable pageable);
 }
--- backend/src/main/java/minionz/backend/scrum/task/TaskRepository.java
+++ backend/src/main/java/minionz/backend/scrum/task/TaskRepository.java
@@ -1,9 +1,11 @@
 package minionz.backend.scrum.task;
 
 import minionz.backend.scrum.task.model.Task;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
@@ -19,6 +21,15 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
            "JOIN FETCH Sprint s ON t.sprint = s " +
            "WHERE tp.user.userId = :userId AND s.endDate > CURRENT_TIMESTAMP")
    List<Task> findMyTask(Long userId);
+
+
+    @Query("SELECT t FROM Task t " +
+            "JOIN FETCH t.sprint s " +
+            "JOIN s.workspace w " +
+            "WHERE w.workspaceId = :workspaceId " +
+            "AND t.startDate <= :endDate " +
+            "AND t.endDate >= :startDate ORDER BY t.endDate ASC ")
+    List<Task> findUpcomingTasks(Long workspaceId, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
 }
 
 
