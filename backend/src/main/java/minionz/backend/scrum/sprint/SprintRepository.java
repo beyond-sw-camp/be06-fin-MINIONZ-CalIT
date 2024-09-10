3d7277dad41cf8b2944e24aa570ac1e6bf986b8c
yunseul <siddl333@naver.com>
2024-09-10 15:54:16 +0900

    [Feat] SCHEDULE_001 - 워크스페이스 월별 스케줄 조회를 위한 jpql 쿼리 구현

--- backend/src/main/java/minionz/backend/scrum/sprint/SprintRepository.java
+++ backend/src/main/java/minionz/backend/scrum/sprint/SprintRepository.java
@@ -2,9 +2,19 @@ package minionz.backend.scrum.sprint;
 
 import minionz.backend.scrum.sprint.model.Sprint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SprintRepository extends JpaRepository<Sprint, Long> {
     List<Sprint> findAllByWorkspaceWorkspaceId(Long id);
+
+    @Query("SELECT s FROM Sprint s " +
+            "JOIN s.workspace w " +
+            "WHERE w.workspaceId = :workspaceId " +
+            "AND s.startDate <= :endDate " +
+            "AND s.endDate >= :startDate")
+    List<Sprint> findSprintsInMonth(Long workspaceId, LocalDateTime startDate, LocalDateTime endDate);
+
 }
