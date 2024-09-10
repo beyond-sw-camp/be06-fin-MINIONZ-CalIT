57976938a80d0e8ad0762e0e1e1bdaffad477fff
yunseul <siddl333@naver.com>
2024-09-10 17:36:48 +0900

    [Feat] SCHEDULE_004 - 개인 주별 캘린더 조회 시 응답 메세지 구현

--- backend/src/main/java/minionz/backend/common/responses/BaseResponseStatus.java
+++ backend/src/main/java/minionz/backend/common/responses/BaseResponseStatus.java
@@ -22,7 +22,7 @@ public enum BaseResponseStatus {
      */
     MY_WORKSPACE_READ_SUCCESS(true, 3001, "나의 워크스페이스 목록 조회를 성공했습니다."),
     MY_MONTHLY_READ_SUCCESS(true, 3002, "나의 월간 캘린더 조회에 성공했습니다."),
-
+    MY_WEEKLY_READ_SUCCESS(true, 3003, "나의 주간 캘린더 조회에 성공했습니다."),
 
     /**
      * 4000: 워크스페이스
