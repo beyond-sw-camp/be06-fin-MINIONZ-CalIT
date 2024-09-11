0f95f6010c372f9db71e8b801200668d181f4128
yunseul <siddl333@naver.com>
2024-09-11 16:46:37 +0900

    [Feat] DASHBOARD_001 - 워크스페이스 번다운 차트 조회시 응답 dto 구현

---
+++ backend/src/main/java/minionz/backend/scrum/dashboard/model/response/ReadBurndownResponse.java
@@ -0,0 +1,17 @@
+package minionz.backend.scrum.dashboard.model.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+public class ReadBurndownResponse {
+    private Long sprintId;
+    private String sprintName;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private List<BurnTaskResponse> tasks;
+}
