51a138a03cca3b301b19aeec0ae4e0a4e120ea6d
seongxun <rjqlrl30@naver.com>
2024-09-20 15:19:22 +0900

    [Refactor] 로그인 로직 수정

--- backend/src/main/java/minionz/backend/config/filter/LoginFilter.java
+++ backend/src/main/java/minionz/backend/config/filter/LoginFilter.java
@@ -56,6 +56,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
         GrantedAuthority auth = iterator.next();
         String role = auth.getAuthority();
+        System.out.println(role);
         String token = jwtUtil.createToken(username, customUserDetails.getUser().getUserId(), role);
         Cookie aToken = new Cookie("ATOKEN", token);
         aToken.setHttpOnly(true);
