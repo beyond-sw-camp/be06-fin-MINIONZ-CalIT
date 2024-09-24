310edef4131acad38cdbe605708096d94a30d6a8
yunseul <siddl333@naver.com>
2024-09-24 17:22:15 +0900

    [Rename] 파일 이름 수정

--- backend/src/main/java/minionz/backend/health/HealthController.java
+++ backend/src/main/java/minionz/backend/health/HealthController.java
@@ -3,7 +3,7 @@ package minionz.backend.health;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class TestController {
+public class HealthController {
     @RequestMapping(method = RequestMethod.GET, value = "/health")
     public String test() {
         return "health";
