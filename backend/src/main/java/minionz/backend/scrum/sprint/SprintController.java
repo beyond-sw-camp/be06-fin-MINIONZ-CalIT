4c3610bdb3af228311f101c9e3f2b6da99c37312
yunseul <siddl333@naver.com>
2024-10-07 18:48:38 +0900

    [Feat] 토큰 재발행 되도록 추가 구현

--- backend/src/main/java/minionz/backend/scrum/sprint/SprintController.java
+++ backend/src/main/java/minionz/backend/scrum/sprint/SprintController.java
@@ -1,6 +1,7 @@
 package minionz.backend.scrum.sprint;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import minionz.backend.common.exception.BaseException;
 import minionz.backend.common.responses.BaseResponse;
@@ -9,6 +10,7 @@ import minionz.backend.scrum.sprint.model.request.CreateSprintRequest;
 import minionz.backend.scrum.sprint.model.request.UpdateSprintStatusRequest;
 import minionz.backend.scrum.sprint.model.response.ReadAllSprintResponse;
 import minionz.backend.scrum.sprint.model.response.ReadSprintResponse;
+import minionz.backend.user.UserService;
 import minionz.backend.user.model.CustomSecurityUserDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -21,13 +23,15 @@ import java.util.List;
 public class SprintController {
 
   private final SprintService sprintService;
+  private final UserService userService;
 
   @PostMapping("/{workspaceId}")
   public BaseResponse<BaseResponseStatus> createSprint(
-      @AuthenticationPrincipal CustomSecurityUserDetails customUserDetails, @RequestBody CreateSprintRequest request) {
+          @AuthenticationPrincipal CustomSecurityUserDetails customUserDetails, @RequestBody CreateSprintRequest request, HttpServletResponse response) {
 
     try {
       sprintService.createSprint(customUserDetails.getUser(), request);
+      userService.sendNewToken(customUserDetails, response);
     } catch (BaseException e) {
       return new BaseResponse<>(e.getStatus());
     } catch (JsonProcessingException e) {
--- backend/src/main/java/minionz/backend/scrum/workspace/WorkspaceController.java
+++ backend/src/main/java/minionz/backend/scrum/workspace/WorkspaceController.java
@@ -1,12 +1,14 @@
 package minionz.backend.scrum.workspace;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import minionz.backend.common.exception.BaseException;
 import minionz.backend.common.responses.BaseResponseStatus;
 import minionz.backend.common.responses.BaseResponse;
 import minionz.backend.scrum.workspace.model.request.CreateWorkspaceRequest;
 import minionz.backend.scrum.workspace.model.response.ReadWorkspaceResponse;
+import minionz.backend.user.UserService;
 import minionz.backend.user.model.CustomSecurityUserDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ import java.util.List;
 @RequestMapping("/workspace")
 public class WorkspaceController {
     private final WorkspaceService workspaceService;
+    private final UserService userService;
 
     @PostMapping("")
     public BaseResponse<BaseResponseStatus> createWorkspace(
@@ -30,10 +33,11 @@ public class WorkspaceController {
 
     @PatchMapping("/accept/{workspaceId}")
     public BaseResponse<BaseResponseStatus> acceptWorkspace(
-            @AuthenticationPrincipal CustomSecurityUserDetails customUserDetails, @PathVariable Long workspaceId) {
+            @AuthenticationPrincipal CustomSecurityUserDetails customUserDetails, @PathVariable Long workspaceId, HttpServletResponse response) {
 
         try {
             workspaceService.accept(customUserDetails.getUser(), workspaceId);
+            userService.sendNewToken(customUserDetails, response);
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
