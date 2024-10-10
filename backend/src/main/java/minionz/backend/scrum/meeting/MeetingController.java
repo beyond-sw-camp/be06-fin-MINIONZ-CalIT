8d9a298d7a566e11cbba13ef2401e51eab4e60ee
Yunseul <117149045+yunseul-dev@users.noreply.github.com>
2024-10-10 16:11:52 +0900

    Revert "[Refactor] 회의 생성 기능 수정[#92]"

--- backend/src/main/java/minionz/backend/scrum/meeting/MeetingController.java
+++ backend/src/main/java/minionz/backend/scrum/meeting/MeetingController.java
@@ -5,7 +5,6 @@ import minionz.backend.common.exception.BaseException;
 import minionz.backend.common.responses.BaseResponse;
 import minionz.backend.common.responses.BaseResponseStatus;
 import minionz.backend.scrum.meeting.model.request.CreateMeetingRequest;
-import minionz.backend.scrum.meeting.model.response.CreateMeetingResponse;
 import minionz.backend.scrum.meeting.model.response.ReadAllMeetingResponse;
 import minionz.backend.scrum.meeting.model.response.ReadMeetingResponse;
 import minionz.backend.user.model.CustomSecurityUserDetails;
@@ -21,17 +20,16 @@ public class MeetingController {
   private final MeetingService meetingService;
 
   @PostMapping("/{sprintId}")
-  public BaseResponse<CreateMeetingResponse> createMeeting(
+  public BaseResponse<BaseResponseStatus> createMeeting(
       @AuthenticationPrincipal CustomSecurityUserDetails customUserDetails, @PathVariable Long sprintId,
       @RequestBody CreateMeetingRequest request) {
-    CreateMeetingResponse response;
     try {
-   response = meetingService.createMeeting(customUserDetails.getUser(), request, sprintId);
+      meetingService.createMeeting(customUserDetails.getUser(), request, sprintId);
     } catch (BaseException e) {
       return new BaseResponse<>(e.getStatus());
     }
 
-    return new BaseResponse<>(BaseResponseStatus.MEETING_CREATE_SUCCESS, response);
+    return new BaseResponse<>(BaseResponseStatus.MEETING_CREATE_SUCCESS);
   }
 
   @GetMapping("/{workspaceId}/{meetingId}")
