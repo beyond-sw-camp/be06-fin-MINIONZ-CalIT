4f03eb3ed547460ac66f862b2a53786466960f67
yeon <dutls5367@gmail.com>
2024-10-11 10:39:48 +0900

    [Refactor] 채팅방 목록 및 채팅,파일 보내기 수정

--- frontend/src/view/chat/component/ChatMessage.vue
+++ frontend/src/view/chat/component/ChatMessage.vue
@@ -3,19 +3,28 @@ import space6 from '@/assets/icon/persona/space6.svg';
 import { defineProps } from 'vue';
 
 defineProps({
+  message:{
+    type: Object,
+    default: () => ({}),
+  },
   isOwnMessage: {
     type: Boolean,
     required: true,
   },
   messageContents: {
     type: String,
-    required: true,
+    default: '',
+  },
+  file: {
+    type: Object,
+    default: () => ({}),
   },
   createdAt: {
     type: String,
     required: true,
   },
 });
+
 </script>
 
 <template>
@@ -23,7 +32,11 @@ defineProps({
     <img v-if="!isOwnMessage" class="profile-pic" :src="space6" alt="profile"/>
     <div class="message-content">
       <div class="message-bubble">
-        <p>{{ messageContents || 'No message' }}</p>
+        <img v-if="message.file && message.file.fileUrl && message.file.fileType.startsWith('image/')" :src="message.file.fileUrl" alt="file" style="max-width: 230px"/>
+        <a v-else-if="message.file && message.file.fileUrl" :href="message.file.fileUrl" target="_blank">{{ message.file.fileName || '파일 보기' }}</a>
+
+        <!-- 메시지가 있을 경우 메시지 표시 -->
+        <p v-else>{{ messageContents || 'No message' }}</p>
       </div>
       <span class="timestamp">{{ createdAt || 'No time' }}</span>
     </div>
@@ -38,11 +51,9 @@ defineProps({
   margin-bottom: 15px;
 }
 
+
 .own {
   flex-direction: row-reverse;
-  img{
-    display: none;
-  }
 }
 
 .profile-pic {
