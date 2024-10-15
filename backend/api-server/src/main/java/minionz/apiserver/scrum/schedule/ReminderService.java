e08ea754b6e6966f91a5487cdd7093f645845dca
yunseul <siddl333@naver.com>
2024-10-15 12:32:04 +0900

    [fix] 스프링 배치로 변경하며 코드 변경으로 이한 에러 주석 처리

--- backend/api-server/src/main/java/minionz/apiserver/scrum/schedule/ReminderService.java
+++ backend/api-server/src/main/java/minionz/apiserver/scrum/schedule/ReminderService.java
@@ -22,24 +22,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReminderService {
 
-    private final MeetingRepository meetingRepository;
+//    private final MeetingRepository meetingRepository;
     private final AlarmService alarmService;
     private final TaskRepository taskRepository;
     private final SprintRepository sprintRepository;
 
-    @Scheduled(cron = "0 */10 * * * ?")
-    @Transactional
-    public void sendMeetingReminders() {
-        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-        List<Meeting> upcomingMeetings = meetingRepository.findMeetingByStartDateAfter(now);
-
-        for (Meeting meeting : upcomingMeetings) {
-            if (meeting.getStartDate().minusMinutes(10).truncatedTo(ChronoUnit.MINUTES).isEqual(now)) {
-                List<Long> participants = meeting.getMeetingParticipations().stream().map(meetingParticipationRepository -> meetingParticipationRepository.getUser().getUserId()).toList();
-                alarmService.sendScheduledEventsToClients(participants, 6L, meeting.getMeetingId());
-            }
-        }
-    }
+//    @Scheduled(cron = "0 */10 * * * ?")
+//    @Transactional
+//    public void sendMeetingReminders() {
+//        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+//        List<Meeting> upcomingMeetings = meetingRepository.findMeetingByStartDateAfter(now);
+//
+//        for (Meeting meeting : upcomingMeetings) {
+//            if (meeting.getStartDate().minusMinutes(10).truncatedTo(ChronoUnit.MINUTES).isEqual(now)) {
+//                List<Long> participants = meeting.getMeetingParticipations().stream().map(meetingParticipationRepository -> meetingParticipationRepository.getUser().getUserId()).toList();
+//                alarmService.sendScheduledEventsToClients(participants, 6L, meeting.getMeetingId());
+//            }
+//        }
+//    }
 
     @Scheduled(cron = "0 */10 * * * ?")
     @Transactional
