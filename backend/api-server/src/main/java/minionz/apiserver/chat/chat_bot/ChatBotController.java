da039b464a143fdf824256080a914e5315cef45d
yeon <dutls5367@gmail.com>
2024-10-15 15:13:46 +0900

    [Feat] CHATBOT_001 - 챗봇 n8n 워크플로우 구현

--- backend/api-server/src/main/java/minionz/apiserver/chat/chat_bot/ChatBotController.java
+++ backend/api-server/src/main/java/minionz/apiserver/chat/chat_bot/ChatBotController.java
@@ -2,20 +2,51 @@ package minionz.apiserver.chat.chat_bot;
 
 import lombok.RequiredArgsConstructor;
 import minionz.apiserver.chat.chat_bot.model.request.ChatBotRequest;
+import minionz.apiserver.common.exception.BaseException;
+import minionz.apiserver.common.responses.BaseResponse;
+import minionz.apiserver.common.responses.BaseResponseStatus;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatBotController {
 
-    private final ChatBotService chatbotService;
+    private final ChatBotService chatBotService;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    // GPT 로 질문을 보내고 응답을 받는 경로
-    @PostMapping("/chatbot/ask-ai")
-    public Mono<String> askAi(@RequestBody ChatBotRequest request) {
-        return chatbotService.askAi(request.getQuestion(), request.getUserId());
+    @MessageMapping("/bot/message")
+    public BaseResponse<String> chatBotMessage(@Payload ChatBotRequest request) {
+        try {
+            if (!request.isFromChatBot()) {
+                Long botQuestionId = chatBotService.saveUserMessage(request);
+                chatBotService.sendMessageToN8n(botQuestionId, request.getMessageContents(), request.getUserId());
+                return new BaseResponse<>(BaseResponseStatus.CHATBOT_MESSAGE_RECEIVED);
+            }
+            return new BaseResponse<>(BaseResponseStatus.CHATBOT_RESPONSE_SAVED);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    // n8n에서 받은 응답
+    @PostMapping("/receiveFromN8n")
+    public BaseResponse<String> receiveFromN8n(@RequestBody Map<String, String> requestData) {
+        try {
+            String message = requestData.get("message");
+//            Long userId = Long.parseLong(requestData.get("userId"));
+            Long botQuestionId = Long.parseLong(requestData.get("botQuestionId"));
+            Long userId = chatBotService.saveChatBotResponse(message, botQuestionId);
+            messagingTemplate.convertAndSend("/subUser/" + userId, message);
+            return new BaseResponse<>(BaseResponseStatus.CHATBOT_RESPONSE_SAVED);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
     }
 }
