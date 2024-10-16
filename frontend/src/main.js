e730f4101ae8558fef26ad87a5317e03b2db4a35
seongxun <rjqlrl30@naver.com>
2024-10-16 11:54:12 +0900

    [Refactor] main.js의 알림 요청 전송 URL을 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -22,7 +22,7 @@ function connectEventSource() {
         eventSource.close(); // 기존 연결 종료
     }
 
-    eventSource = new EventSource(`https://calit.kro.kr/alarm/connect/${userId}`);
+    eventSource = new EventSource(`https://calit.kro.kr/api/alarm/connect/${userId}`);
 
     eventSource.onmessage = (event) => {
         console.log('New message:', event.data);
