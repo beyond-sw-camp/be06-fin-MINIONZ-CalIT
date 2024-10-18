57c91e930fbba45955f9ee7a41e0ea2503ad9ad7
seongxun <rjqlrl30@naver.com>
2024-10-18 12:57:20 +0900

    [Refactor] SSE 연결 경로 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,14 @@
-import '@/assets/style/styles.css';
-import 'vuetify/styles';
-import 'notyf/notyf.min.css';
+import "@/assets/style/styles.css";
+import "vuetify/styles";
+import "notyf/notyf.min.css";
 
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './App.vue';
-import router from './router';
-import vuetify from './plugins/vuetify';
-import { Notyf } from 'notyf';
-import { useUserStore } from '@/stores/user/useUserStore';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import { Notyf } from "notyf";
+import { useUserStore } from "@/stores/user/useUserStore";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -18,34 +18,38 @@ const userStore = useUserStore();
 const userId = userStore.user.value?.idx;
 let eventSource;
 function connectEventSource() {
-  if (eventSource) {
-    eventSource.close(); // 기존 연결 종료
+  const path = window.location.pathname;
+  console.log(path);
+  if (path.startsWith("/my") || path.startsWith("/workspace")) {
+    if (eventSource) {
+      eventSource.close(); // 기존 연결 종료
+    }
+
+    eventSource = new EventSource(
+      `https://calit.kro.kr/api/alarm/connect/${userId}`
+    );
+
+    eventSource.onmessage = (event) => {
+      console.log("New message:", event.data);
+      console.log("메세지가 도착했어요!");
+      const data = JSON.parse(event.data);
+      notyf.success(data.AlarmContents);
+    };
+
+    eventSource.onerror = () => {
+      console.error("SSE 연결이 끊어졌습니다. 재연결 시도 중...");
+      eventSource.close(); // 현재 연결 종료
+      setTimeout(connectEventSource, 20000); // 20초 후 재연결 시도
+    };
   }
-
-  eventSource = new EventSource(
-    `https://calit.kro.kr/api/alarm/connect/${userId}`
-  );
-
-  eventSource.onmessage = (event) => {
-    console.log('New message:', event.data);
-    console.log('메세지가 도착했어요!');
-    const data = JSON.parse(event.data);
-    notyf.success(data.AlarmContents);
-  };
-
-  eventSource.onerror = () => {
-    console.error('SSE 연결이 끊어졌습니다. 재연결 시도 중...');
-    eventSource.close(); // 현재 연결 종료
-    setTimeout(connectEventSource, 200000); // 1초 후 재연결 시도
-  };
 }
 
 connectEventSource();
 
 router.onError((error) => {
-  console.error('Router error:', error);
+  console.error("Router error:", error);
 });
 app.use(pinia);
 app.use(router);
 app.use(vuetify);
-app.mount('#app');
+app.mount("#app");
