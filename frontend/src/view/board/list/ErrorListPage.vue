b30fe4f758786f7e6380d4213583db1971b9e3d1
chloe_choi <plumxeun@gmail.com>
2024-10-21 14:41:57 +0900

    [Refactor] 주간 페이지 날짜 변환 수정

--- frontend/src/view/board/list/ErrorListPage.vue
+++ frontend/src/view/board/list/ErrorListPage.vue
@@ -3,8 +3,8 @@ import { computed, inject, onMounted, ref,  watch } from 'vue';
 import { useErrorStore } from '@/stores/board/useErrorStore';
 import Pagination from '@/common/component/PaginationComponent.vue';
 import BoardList from '@/common/component/Board/ErrorBoardList.vue';
-import SearchComponent from '@/common/component/SearchComponent.vue';
 import { useRoute } from 'vue-router';
+import Multiselect from "vue-multiselect";
 
 const route = useRoute();
 const workspaceId = route.params.workspaceId;
@@ -104,13 +104,25 @@ watch(searchKeyword, async () => {
 
 <template>
   <div class="board-list-container">
+
     <div v-if="postList.length > 0">
       <div class="header">
-        <SearchComponent
-            :link="`/workspace/${workspaceId}/scrum/board/error/create`"
-            @search="searchKeyword = $event"
-            @update:selectedLanguage="filterByLanguage"
-        />
+        <div class="toolbar">
+          <div class="filter-search">
+            <div class="filter">
+              <multiselect v-model="selectedLanguage" :options="['JAVA', 'C', 'PYTHON', 'JS', 'SQL']" @input="$emit('update:selectedLanguage', selectedLanguage)" />
+            </div>
+            <div class="search">
+              <input type="text" class="search-input" placeholder="Search..." />
+              <span class="search-icon">🔍</span>
+            </div>
+          </div>
+
+          <!-- Create 버튼 -->
+          <router-link :to="`/workspace/${workspaceId}/scrum/board/error/create`" class="create-button">
+            <span class="create-icon">+</span> Create
+          </router-link>
+        </div>
       </div>
       <BoardList
           :items="postList"
@@ -140,6 +152,71 @@ watch(searchKeyword, async () => {
 </template>
 
 <style scoped>
+.toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+  background-color: #fff;
+  border-radius: 8px;
+}
+
+.filter-search {
+  display: flex;
+  align-items: center;
+}
+
+.filter-button {
+  padding: 8px 16px;
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  background-color: #f8f8f8;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+}
+
+.dropdown-icon {
+  margin-left: 5px;
+}
+
+.search {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.search-input {
+  padding: 8px 8px 8px 30px;
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  outline: none;
+  width: 200px;
+  background-color: #f8f8f8;
+}
+
+.search-icon {
+  position: absolute;
+  margin-left: 10px;
+}
+
+.create-button {
+  text-decoration: none;
+  background-color: #e0e8ff;
+  color: #666daf;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+}
+
+.create-icon {
+  margin-right: 5px;
+}
+
 .initial-wrap {
   display: flex;
   flex-direction: column;
